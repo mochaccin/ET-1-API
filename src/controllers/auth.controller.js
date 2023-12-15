d9e0ae782cb6476bed8a5a228daad20f96b991c9
mochaccin <mochaccindotcl@gmail.com>
2023-12-15 12:55:59 -0300

    fixed getMe method

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,40 +3,39 @@ import bcrypt from "bcrypt";
 import { generateToken } from "../utils/jwt.js";
 
 async function getMe(req, res) {
-	const userId = req.userId;
-	try {
-		const user = await userModel.findById(userId);
+  const userId = req.id;
+  try {
+    const user = await userModel.findById(userId);
 
-		return res.status(200).json(user);
-	} catch (error) {
-		response.status(404).send({ error });
-	}
+    return res.status(200).json(user);
+  } catch (error) {
+    response.status(404).send({ error });
+  }
 }
 
-
 async function login(request, response) {
-    const user = await userModel
-      .findOne({ email: request.body.email })
-      .select("+password")
-      .exec();
-  
-    if (!user) {
-      return response.status(404).send({ message: "User not found" });
-    }
-  
-    const passwordIsCorrect = await bcrypt.compare(
-      request.body.password,
-      user.password
-    );
-  
-    if (!passwordIsCorrect) {
-      return response.status(400).send({ message: "Incorrect password" });
-    }
-  
-    const token = generateToken(user);
-    const { role } = user;
-  
-    return response.status(200).send({ token, role });
+  const user = await userModel
+    .findOne({ email: request.body.email })
+    .select("+password")
+    .exec();
+
+  if (!user) {
+    return response.status(404).send({ message: "User not found" });
   }
+
+  const passwordIsCorrect = await bcrypt.compare(
+    request.body.password,
+    user.password
+  );
+
+  if (!passwordIsCorrect) {
+    return response.status(400).send({ message: "Incorrect password" });
+  }
+
+  const token = generateToken(user);
+  const { role } = user;
+
+  return response.status(200).send({ token, role });
+}
 
 export { getMe, login };
